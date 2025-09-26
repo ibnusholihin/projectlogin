238729f740973a3dd1c7779041e74ec1a3ebeaa1
ibnusholihin <ibensholihin1@gmail.com>
2025-09-26 17:02:29 +0700

    mendesain halaman login

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Form Login</h2>
+    <?php if (!empty($error)) echo "<p style='color:red; '>$error</p>"; ?>
+    <form method="post">
+        Username: <input type="text" name="username" required><br><br>
+        Password: <input type="password" name="password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+
+</html>
